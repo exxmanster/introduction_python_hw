17ab9ecc6bf67cd7dedcb65505a3afad50849f42
exxmanster <exxmanster@gmail.com>
2018-08-07 12:28:38 +0300

    task_2

---
+++ task_2.py
@@ -0,0 +1,13 @@
+#Task_2
+
+####Найти результат выражения для произвольных значений a,b: (a2 + b2) % 2
+
+
+import math
+
+
+a = 4
+b = 8
+ab_result = (pow(a, 2) + pow(b, 2))%2
+
+print('TheResult of (a**2 + b**2)%2', 'for a = %.2f and b = %.2f is equel to %d' %(a, b, ab_result))
