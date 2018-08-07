d5189b03e540421e0a35479acfbc60d32f0321d8
exxmanster <exxmanster@gmail.com>
2018-08-07 13:02:33 +0300

    task_3

---
+++ task_3.py
@@ -0,0 +1,12 @@
+#Task #3
+
+###Найти результат выражения для произвольных значений a,b,c: ( a + b ) / 12 * c % 4 + b
+
+
+a = 125
+b = 234
+c = 50
+abc_result = (a + b)/12*c%4 + b
+
+
+print('Result of ( a + b )/12*c%4 + b','for a = %.2f, b = %.2f, c = %.2f is %.3f' %(a, b, c, abc_result))
