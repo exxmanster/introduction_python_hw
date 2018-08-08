49bd4ff9f9e709fa8acf0bad5820eaefa9f2ef41
exxmanster <exxmanster@gmail.com>
2018-08-08 15:06:31 +0300

    Homework

--- task_6.py
+++ task_6.py
@@ -10,6 +10,6 @@ c = 7
 
 if b != 0:
     abc_result = math.pow(math.log1p(1 + c)/(-b), 4) + math.fabs(a)
-    print('The result of (ln(1 + c) / -b)**4 + |a| for a = %d, b = %d, c = %d is %.10f'
+    print('The result of (ln(1 + c) / -b)4 + |a| for a = %d, b = %d, c = %d is equal to %.10f'
     %(a, b, c, abc_result))
 else: print('Error: division by ZERO!!!')
